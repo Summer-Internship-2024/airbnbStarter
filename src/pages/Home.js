624413d177356947a3adef13701d12eb49c63d92
James Leahy Stephens <jaystephens1124@gmail.com>
2024-05-19 17:36:40 -0400

    fixing function names

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,7 @@ import Cards from "../components/Cards";
 import Filter from "../components/Filter";
 import Header from "../components/Header";
 
-function App() {
+export default function Home() {
   const [selectedFilter, setSelectedFilter] = useState(0);
   return (
     <div className="App">
@@ -18,5 +18,3 @@ function App() {
     </div>
   );
 }
-
-export default App;
