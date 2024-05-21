3af1354489246c6ce69280a0882036aace09f3e9
James Leahy Stephens <jaystephens1124@gmail.com>
2024-05-21 14:24:15 -0400

    setting yyup API

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Home from "./pages/Home";
 import Login from "./pages/Login";
 import { HashRouter as Router,Routes,Route } from "react-router-dom";
 
+var account_id = -1;
+
 function App() {
   return (
     <Router>
